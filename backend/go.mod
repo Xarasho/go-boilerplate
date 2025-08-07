bb2ae513f86e0301ef33859fe571741519946d72
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-08-07 05:30:49 -0600

    Added config.go

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,20 @@
 module github.com/Xarasho/go-boilerplate
 
 go 1.24.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/knadh/koanf/providers/env v1.1.0
+	github.com/knadh/koanf/v2 v2.2.2
+	github.com/rs/zerolog v1.34.0
+)
+
+require (
+	github.com/go-viper/mapstructure/v2 v2.3.0 // indirect
+	github.com/knadh/koanf/maps v0.1.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ backend/internal/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/v2"
+	"github.com/rs/zerolog"
+)
+
+type Config struct {
+	Primary  Primary        `koanf:"primary" validate:"required"`
+	Server   ServerConfig   `koanf:"server" validate:"required"`
+	Database DatabaseConfig `koanf:"database" validate:"required"`
+	Auth     AuthConfig     `koanf:"auth" validate:"required"`
+}
+
+type Primary struct {
+	Env string `koanf:"env" validate:"required"`
+}
+
+type ServerConfig struct {
+	Port               string      `koanf:"port" validate:"required"`
+	ReadTimeout        int         `koanf:"read_timeout" validate:"required"`
+	WriteTimeout       int         `koanf:"write_timeout" validate:"required"`
+	IdleTimeout        int         `koanf:"idle_timeout" validate:"required"`
+	CORSAllowedOrigins []string    `koanf:"cors_allowed_origins" validate:"required"`
+	Redis              RedisConfig `koanf:"redis" validate:"required"`
+}
+
+type DatabaseConfig struct {
+	Host            string `koanf:"host" validate:"required"`
+	Port            int    `koanf:"port" validate:"required"`
+	User            string `koanf:"host" validate:"required"`
+	Password        string `koanf:"host" validate:"required"`
+	Name            string `koanf:"host" validate:"required"`
+	SSLMode         string `koanf:"host" validate:"required"`
+	MaxOpenConns    int    `koanf:"host" validate:"required"`
+	MaxIdleConns    int    `koanf:"host" validate:"required"`
+	ConnMaxLifetime int    `koanf:"host" validate:"required"`
+	ConnMaxIdleTime int    `koanf:"host" validate:"required"`
+}
+
+type RedisConfig struct {
+	Address string `koanf:"address" validate:"required"`
+}
+
+type AuthConfig struct {
+	SecretKey string `koanf:"secret_key" validate:"required"`
+}
+
+func LoadConfig() (*Config, error) {
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+
+	k := koanf.New(".")
+
+	err := k.Load(env.Provider("BOILERLATE_", ".", func(s string) string {
+		return strings.ToLower(strings.TrimPrefix(s, "BOILERLATE_"))
+	}), nil)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("could not load initial env variables")
+	}
+
+	mainConfig := &Config{}
+
+	err = k.Unmarshal("", mainConfig)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("could not unmarshal main config")
+	}
+
+	return mainConfig, nil
+}
